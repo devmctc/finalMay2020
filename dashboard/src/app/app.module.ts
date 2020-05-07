f2f95b8bb7f9084a59ba2fc64d46a34839ee24dd
Cammie Love <love.cammie@mail.com>
2020-05-07 10:58:24 -0500

    added layouts

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
